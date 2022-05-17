446962c474e741891c58199560c2465460aa1a59
Nikolay Gorbunov <gorbunov.nv13@gmail.com>
2022-05-17 18:10:58 +0300

    Update subscribe

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,16 @@ Rails.application.routes.draw do
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "posts#index"
+  get "/feed", to: "posts#feed"
 
   resources :posts do
     resources :comments
     resources :likes
   end
   resources :subscribes
+  resources :users, only: [:show] do
+    resources :subscribes, only:[:create, :destroy]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
